3497954bdeef32eb4b473bb77486d46715c3c197
Neel Pandruvada <neel.pandruvada@mcvts.org>
2024-06-10 09:20:26 -0400

    enemy not working  :(((

--- Scripts/Turtle.gd
+++ Scripts/Turtle.gd
@@ -1,11 +1,11 @@
 extends Area2D
 
 var player
-var speed = 1.0  # adjust this value to control the enemy's speed
+var speed = 100.0  # adjust this value to control the enemy's speed
 
 func _ready():
 	player = $"../Player"
 
 func _physics_process(delta):
 	var direction = (player.position - position).normalized()
-	position += direction
+	position += (direction * speed) * delta
